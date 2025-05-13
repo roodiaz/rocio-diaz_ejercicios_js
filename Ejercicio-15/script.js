4e01fee295fce873042f91f1d550335cfd9a201f
roodiaz <roabigail.diaz@gmail.com>
2025-05-13 20:11:25 -0300

    add - ejercicio 15

---
+++ Ejercicio-15/script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn");
+let image = document.getElementById("image");
+
+// rutas de las imágenes
+const imagenes = [
+  "../img/imagen 1.jpg",
+  "../img/imagen 2.jpg",
+  "../img/imagen 3.jpg"
+];
+
+btn.addEventListener("click", function () {
+  // genero indice aleatorio
+  const indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+
+  image.src = imagenes[indiceAleatorio];
+});
+
