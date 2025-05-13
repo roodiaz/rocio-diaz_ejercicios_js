529fe68add7c2d8c49c6a15d4c73fe0ac69b0cfe
roodiaz <roabigail.diaz@gmail.com>
2025-05-13 19:19:14 -0300

    add - ejercicio nro 10

---
+++ Ejercicio-10/script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById("input");
+let h2 = document.getElementById("h2");
+
+input.addEventListener("input", function () {
+  let inputValue = input.value;
+  h2.textContent = inputValue;
+});
