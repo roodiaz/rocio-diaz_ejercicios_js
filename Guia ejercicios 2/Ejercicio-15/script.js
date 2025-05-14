4b5125106c58cd3c6453552083317b3ee82c697b
roodiaz <roabigail.diaz@gmail.com>
2025-05-13 22:01:35 -0300

    fix - ruta imagenes

--- Guia ejercicios 2/Ejercicio-15/script.js
+++ Guia ejercicios 2/Ejercicio-15/script.js
@@ -3,9 +3,9 @@ let image = document.getElementById("image");
 
 // rutas de las imágenes
 const imagenes = [
-  "../img/imagen 1.jpg",
-  "../img/imagen 2.jpg",
-  "../img/imagen 3.jpg"
+  "../../img/imagen 1.jpg",
+  "../../img/imagen 2.jpg",
+  "../../img/imagen 3.jpg"
 ];
 
 btn.addEventListener("click", function () {
